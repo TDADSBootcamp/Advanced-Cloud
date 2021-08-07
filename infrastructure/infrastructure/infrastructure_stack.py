382f8d05e302c5e74c45766dd27d52de8775a11f
Paul Brabban <paul.brabban@gmail.com>
2021-08-07 21:59:01 +0100

    sort out ut student access setup

--- infrastructure/infrastructure/infrastructure_stack.py
+++ infrastructure/infrastructure/infrastructure_stack.py
@@ -1,3 +1,9 @@
+import random
+import string
+import os
+import sys
+import json
+
 from aws_cdk import core as cdk
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_iam as iam
@@ -7,79 +13,103 @@ from aws_cdk import aws_sagemaker as sm
 
 import infrastructure.emr_cluster as emr_cluster
 
+
+def random_string(length: int) -> str:
+  '''
+    >>> import re
+    >>> assert re.match(r'^[a-z]+$', random_string(100))
+    '''
+  return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+
+
 class InfrastructureStack(cdk.Stack):
 
-    _students = 4
+  def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    student_count = os.environ['STUDENT_COUNT']
+    billing_alert_emails = os.environ['BILLING_ALERT_EMAILS'].split(',')
 
-        example_bucket = s3.Bucket(self, 'example-bucket',
-            versioned=True)
+    example_bucket = s3.Bucket(self, 'example-bucket', versioned=True)
 
-        student_group = iam.Group(self, 'students', managed_policies=[
-            iam.ManagedPolicy.from_aws_managed_policy_name('AmazonAthenaFullAccess')
+    student_group = iam.Group(
+        self,
+        'students',
+        group_name='students',
+        managed_policies=[
+            iam.ManagedPolicy.from_aws_managed_policy_name(
+                'AmazonAthenaFullAccess')
         ])
 
-        student_group.add_to_policy(iam.PolicyStatement(
-            actions=['s3:*'],
-            resources=[example_bucket.bucket_arn]
-        ))
+    student_group.add_to_policy(
+        iam.PolicyStatement(actions=['s3:*'],
+                            resources=[example_bucket.bucket_arn]))
 
-        student_iam_accounts = [
-            iam.User(self, f'student_{idx}', groups=[student_group])
-            for idx in range(self._students)
-        ]
+    with open(os.path.join(sys.path[0],
+                           'user_credentials_policy.json')) as policy_json:
+      student_group.attach_inline_policy(
+          iam.Policy(self,
+                     'manage-creds',
+                     document=iam.PolicyDocument.from_json(
+                         json.load(policy_json))))
 
-        covid_data_lake_template = cfn_inc.CfnInclude(self, 'covid-data-lake-glue',
-            template_file='CovidLakeStack.template.json',
-            preserve_logical_ids=False)
+    covid_data_lake_template = cfn_inc.CfnInclude(
+        self,
+        'covid-data-lake-glue',
+        template_file='CovidLakeStack.template.json',
+        preserve_logical_ids=False)
 
-        budget = budgets.CfnBudget(self, 'budget',
-            budget=budgets.CfnBudget.BudgetDataProperty(
-                budget_type='COST',
-                time_unit='MONTHLY',
-                budget_limit=budgets.CfnBudget.SpendProperty(amount=10, unit='USD')
-            ),
-            notifications_with_subscribers=[
-                budgets.CfnBudget.NotificationWithSubscribersProperty(
-                    notification=budgets.CfnBudget.NotificationProperty(
-                        comparison_operator='GREATER_THAN',
-                        notification_type='ACTUAL',
-                        threshold=80
-                    ),
-                    subscribers=[
-                        budgets.CfnBudget.SubscriberProperty(
-                            subscription_type='EMAIL',
-                            address='paul@tempered.works'
-                        )
-                    ]
-                )
-            ]
-        )
+    budget = budgets.CfnBudget(
+        self,
+        'budget',
+        budget=budgets.CfnBudget.BudgetDataProperty(
+            budget_type='COST',
+            time_unit='MONTHLY',
+            budget_limit=budgets.CfnBudget.SpendProperty(amount=10,
+                                                         unit='USD')),
+        notifications_with_subscribers=[
+            budgets.CfnBudget.NotificationWithSubscribersProperty(
+                notification=budgets.CfnBudget.NotificationProperty(
+                    comparison_operator='GREATER_THAN',
+                    notification_type='ACTUAL',
+                    threshold=80),
+                subscribers=[
+                    budgets.CfnBudget.SubscriberProperty(
+                        subscription_type='EMAIL', address=targetAddress)
+                    for targetAddress in billing_alert_emails
+                ])
+        ])
 
-        spark = emr_cluster.EMRClusterStack(self, 'emr_cluster',
-            s3_log_bucket=example_bucket.bucket_name,
-            s3_script_bucket=example_bucket.bucket_name)
+    spark = emr_cluster.EMRClusterStack(
+        self,
+        'emr_cluster',
+        s3_log_bucket=example_bucket.bucket_name,
+        s3_script_bucket=example_bucket.bucket_name)
 
-        notebook_role = iam.Role(self, 'notbooks_access_role',
-            assumed_by=iam.ServicePrincipal('sagemaker'),
-            managed_policies=[
-                iam.ManagedPolicy.from_aws_managed_policy_name('AmazonSageMakerFullAccess')
-            ])
+    notebook_role = iam.Role(self,
+                             'notbooks_access_role',
+                             assumed_by=iam.ServicePrincipal('sagemaker'),
+                             managed_policies=[
+                                 iam.ManagedPolicy.from_aws_managed_policy_name(
+                                     'AmazonSageMakerFullAccess')
+                             ])
 
-        notebook_policy = iam.Policy(self, 'notebook_access_policy',
-            policy_name='notebook_access_policy',
-            statements=[
-                iam.PolicyStatement(actions = ['s3:*'], resources=[example_bucket.bucket_arn])
-            ]).attach_to_role(notebook_role)
+    notebook_policy = iam.Policy(self,
+                                 'notebook_access_policy',
+                                 policy_name='notebook_access_policy',
+                                 statements=[
+                                     iam.PolicyStatement(
+                                         actions=['s3:*'],
+                                         resources=[example_bucket.bucket_arn])
+                                 ]).attach_to_role(notebook_role)
 
-        instance = sm.CfnNotebookInstance(self, 'notebook',
-            instance_type='ml.t2.medium',
-            volume_size_in_gb=5,
-            notebook_instance_name='notebook',
-            role_arn = notebook_role.role_arn
-        )
+    instance = sm.CfnNotebookInstance(self,
+                                      'notebook',
+                                      instance_type='ml.t2.medium',
+                                      volume_size_in_gb=5,
+                                      notebook_instance_name='notebook',
+                                      role_arn=notebook_role.role_arn)
+
 
 app = cdk.App()
 
---
+++ infrastructure/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Pre-reqs: cdk, member AWS account, admin-level role in target account with org account able to assume, ~/.aws/config with target role configured"
+
+export ACCOUNT_ID=$1
+export AWS_PROFILE=$2
+export STUDENT_COUNT=$3
+export BILLING_ALERT_EMAILS=$4
+export REGION=${5:-eu-west-2}
+
+aws iam update-account-password-policy \
+  --minimum-password-length 20 \
+  --no-require-symbols \
+  --no-require-numbers \
+  --no-require-uppercase-characters \
+  --allow-users-to-change-password \
+  --password-reuse-prevention 10
+
+npx cdk bootstrap aws://${ACCOUNT_ID}/${REGION}
+
+npx cdk deploy
